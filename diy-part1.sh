ad89eb77e31dbbd1ebd9ac707ca88e48ff139366
nicker1982 <126138783+nicker1982@users.noreply.github.com>
2025-02-16 14:40:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,8 +8,8 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
+#自定义三方插件
+git clone https://github.com/momokind/luci-app-hypermodem.git package/lean/luci-app-hypermodem
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
